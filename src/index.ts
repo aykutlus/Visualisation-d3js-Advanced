59b3fd9a2b31e6f62f23864198c7a2512d2c2678
aykutlus <sedaaykutlu@gmail.com>
2020-03-28 12:32:49 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,121 @@
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
+const spainjson = require("./spain.json");
+const d3Composite = require("d3-composite-projections");
+import { latLongCommunities } from "./communities";
+import { initial, now, Numbers } from "./stats";
+
+const calculateRadiusBasedOnAffectedCases = (comunidad: string, data: Numbers[]) => {
+  const entry = data.find(item => item.name === comunidad);
+
+  /*const maxAffected = data.reduce(
+    (max, item) => (item.value > max ? item.value : max),
+    0
+  );*/
+
+  const affectedRadiusScale = d3
+    .scaleLinear()
+    .domain([0, 9000])
+    .range([5, 40]); // 50 pixel max radius, we could calculate it relative to width and height
+
+  return entry ? affectedRadiusScale(entry.value) : 0;
+};
+
+const changecolorbasedoninfectedcase = (comunidad: String, data: Numbers[]) => {
+
+  const entry = data.find(item => item.name === comunidad);
+
+  console.log(entry)
+
+  var colorScheme = d3.schemePurples[7];
+  var color = d3.scaleThreshold()
+    .domain([0, 1, 10, 50, 100, 500])
+    .range(<any>colorScheme);
+
+  return entry ? color(entry.value) : color(0);
+};
+
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 1024)
+  .attr("height", 800)
+  .attr("style", "background-color: #FBFAF0");
+
+
+const aProjection = d3Composite
+  .geoConicConformalSpain()
+  // Let's make the map bigger to fit in our resolution
+  .scale(3500)
+  // Let's center the map
+  .translate([500, 400]);
+
+const geoPath = d3.geoPath().projection(aProjection);
+
+const geojson = topojson.feature(spainjson, spainjson.objects.ESP_adm1);
+
+svg
+  .selectAll("path")
+  .data(geojson["features"])
+  .enter()
+  .append("path")
+  .attr("class", "country")
+  // data loaded from json file
+  .attr("d", geoPath as any)
+  .attr("fill", (d) => changecolorbasedoninfectedcase(d['properties']['NAME_1'], initial));
+
+
+
+svg
+  .selectAll("circle")
+  .data(latLongCommunities)
+  .enter()
+  .append("circle")
+  .attr("class", "affected-marker")
+  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name, initial))
+  .attr("cx", d => aProjection([d.long, d.lat])[0])
+  .attr("cy", d => aProjection([d.long, d.lat])[1])
+
+
+//update buttons
+const updateCircles = (data: Numbers[]) => {
+  const circles = svg.selectAll("circle");
+  circles
+    .data(latLongCommunities)
+    .merge(circles as any)
+    .transition()
+    .duration(500)
+    .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name, data))
+};
+
+//update color
+const updateColor = (data: Numbers[]) => {
+  console.log("updateColor");
+  const pathCollection = svg.selectAll("path");
+  pathCollection
+    .data(geojson["features"])
+    .enter()
+    .merge(pathCollection as any)
+    .style("fill", function (d: any) {
+      console.log(d);
+      return changecolorbasedoninfectedcase(d.properties.NAME_1, data);
+    })
+    // data loaded from json file
+    .attr("d", geoPath as any);
+};
+
+document
+  .getElementById("Initial date")
+  .addEventListener("click", function handleResultsInitial() {
+    updateCircles(initial)
+    updateColor(initial)
+  });
+
+document
+  .getElementById("Now")
+  .addEventListener("click", function handleResultsNow() {
+    updateCircles(now)
+    updateColor(now)
+  });
+
